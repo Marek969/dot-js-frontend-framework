d9137cb1035b5198b18b2263689dc7063c2421eb
mareksarkisjan1 <volk9693@gmail.com>
2025-09-19 21:25:21 +0300

    upd

--- example/src/components/TodoSection.ts
+++ example/src/components/TodoSection.ts
@@ -1,10 +1,16 @@
 import { h } from "@dotjs/framework";
 import { TaskItem } from "./TaskItem";
+import { Todo } from "../store";
 
-export function TodoSection({ tasks, onDelete }) {
+interface TodoSectionProps {
+  tasks: Todo[];
+  onDelete: (id: number) => void;
+}
+
+export function TodoSection({ tasks }: TodoSectionProps) {
   return h(
     "ul",
     null,
-    tasks.map((todo) => TaskItem({ todo, onDelete }))
+    tasks.map((todo) => TaskItem({ todo }))
   );
 }
